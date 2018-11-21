a9f48cd85bd53fbd22797dcd83f273118005701f
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-11-20 22:43:19 -0300

    atualizando codigo da camera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  photo: string = '';
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  foto() {
+    this.photo = '';
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      // allowEdit: true,
+      // targetWidth: 100,
+      // targetHeight: 100
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photo = base64Image;
+    }, (err) => {
+      console.log(err);
+      
+      // Handle error
+    });
+  }
 }
